fc54f484157959e69fe9aebbdc3ab19b9584bb71
Zaparoli <cristian.zaparoli@sap.com>
2023-02-22 21:29:28 -0300

    adicionada funcao subtracao

--- calculadora.py
+++ calculadora.py
@@ -2,4 +2,10 @@ import sys
 
 print("\nargs:", sys.argv[1], " ", sys.argv[2], " ", sys.argv[3])
 
-print("soma:", int(sys.argv[2]) + int(sys.argv[3]))
+operacao = sys.argv[1]
+
+if (operacao == "+") :
+    print("soma:", int(sys.argv[2]) + int(sys.argv[3])) 
+elif (operacao == "-") :
+    print("subtracao:", int(sys.argv[2]) - int(sys.argv[3]))     
+
