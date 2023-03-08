a508a39fd0d801ceabb1385cccd85b9623f5017d
MateusKirch <silvamateusf@gmail.com>
2023-03-08 20:15:55 -0300

    Inserção "8"

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 from calculadora import *
+from busca import *
 #Seleciona função
 #1 - Calculadora
 #2 - Inverte string
@@ -11,16 +12,20 @@ from calculadora import *
 #8 - Verifica se palavra/caracter existe na string
 #9 - Imprime o número Pi com x casas decimais 
 #Enter - Sair
-print("informe a funcão:\n1 - Calculadora" + 
+print("informe a funcão:\n1 - Calculadora" + "\n8 - Busca" +
       "\nEnter - Sair")
 funcao = input()
 if (funcao == "1"):
     calculate()
+elif (funcao == "8"):
+    busca()
 elif (funcao == ""): 
     pass
 else:
     print("Função não implementada!")
 #Encerra   
 print("Bye!")
+
+
 
 
---
+++ busca.py
@@ -0,0 +1,15 @@
+def busca():
+    lista = [1,2,3,4,5]
+    print("Digite o caracter a ser procurado: ")
+    args = input().split()
+    caractere = int(args[0])
+    if lista[0] == caractere:
+        print("O caractere procurado existe")
+    elif lista[1] == caractere:
+        print("O caractere procurado existe")
+    elif lista[2] == caractere:
+        print("O caractere procurado existe")
+    elif lista[3] == caractere:
+        print("O caractere procurado existe")
+    elif lista[4] == caractere:
+        print("O caractere procurado existe")
