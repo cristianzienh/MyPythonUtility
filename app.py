314ae36cf898f17f53d772aa9099dffc8181768c
vinicius378 <126727289+vinicius378@users.noreply.github.com>
2023-03-08 21:02:26 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import sys
 from calculadora import *
-from po import *
 from busca import *
 from pi import *
 from parouimpar import *
@@ -15,17 +14,22 @@ from parouimpar import *
 #8 - Verifica se palavra/caracter existe na string
 #9 - Imprime o número Pi com x casas decimais 
 #Enter - Sair
-+print("informe a funcão:\n1 - Calculadora" + "\n3 - Ordena caracteres da string " + "\n5 - Palíndromo" + "\n6 - Par ou ímpar" +  "\n8 - Busca" + "\n9 - Pi" +
-     
+print("informe a funcão:\n1 - Calculadora" +"\n3 - Ordena String " +  "\n6 - Par Ou Impar" + "\n8 - Busca" + "\n9 - Pi" +
+      "\n5 - Palíndromo" + 
       "\nEnter - Sair")
 funcao = input()
 if (funcao == "1"):
     calculate()
-elif (funcao == "3"): 
-    ordena()
+elif (funcao == "5"): 
+    palindromo()
+elif (funcao == "8"):
+    busca()
+elif (funcao == "9"):
+    piFunction()
+elif (funcao =="6"):
+    parouimpar()
+elif (funcao == ""): 
+    pass
 else:
-    print("Função não implementada!")
-#Encerra   
+    print("Função não implementada!") 
 print("Bye!")
-
-
